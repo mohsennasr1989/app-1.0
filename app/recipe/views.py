7554c9fda1490d807f745e98e152ca197e200208
mohsennasr1989 <mohsennasr1989@gmail.com>
2020-12-19 23:28:46 +0330

    add recipe module

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from rest_framework import mixins, viewsets
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+from . import serializers
+from ..core.models import Tag
+
+
+class TagViewSet(viewsets.GenericViewSet, mixins.ListModelMixin):
+    """Manage tags in database"""
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (IsAuthenticated,)
+    queryset = Tag.objects.all()
+    serializer_class = serializers.TagSerializer
+
+    def get_queryset(self):
+        """Return objects for the current authenticated user only"""
+        return self.queryset.filter(user=self.request.user).order_by('-name')
+
+
